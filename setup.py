9026eff7bc10a74a1be1f709bcadfc3e41c6932b
Alex Clark <aclark@aclark.net>
2014-01-26 09:47:29 -0500

    Add content

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ setup(
     entry_points={
         'z3c.autoinclude.plugin': 'target = plone',
     },
+    install_requires=[
+# XXX Matplotlib requires numpy, install via pip install -r requirements.txt
+        'matplotlib',
+    ],
     name='plone_matplotlib',
     packages=find_packages(),
 )
